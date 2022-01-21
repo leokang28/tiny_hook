4b3936da1bd721b16282fe6b8ce27e044c65feb0
ksleo <leokang2828@gmail.com>
2022-01-21 05:20:14 -0800

    init: a tiny implemention for react hook

---
+++ index.js
@@ -0,0 +1,85 @@
+// current node to process
+let workInProgressHook;
+// is first load or not
+let isMount = true;
+
+const fiber = {
+  memoizedState: null, // current node's state
+  stateNode: App // pointer to real node which is ReactComponent in React
+};
+
+function schedule() {
+  workInProgressHook = fiber.memoizedState;
+  const app = fiber.stateNode();
+  isMount = false;
+  return app;
+}
+
+function dispatchAction(queue, action) {
+  const update = {
+    action,
+    next: null
+  }
+  if (queue.pending === null) {
+    update.next = update;
+  } else {
+    update.next = queue.pending.next;
+    queue.pending.next = update;
+  }
+  queue.pending = update;
+
+  schedule();
+}
+
+function useState(initialState) {
+  let hook;
+
+  if (isMount) {
+    hook = {
+      queue: {
+        pending: null
+      },
+      memoizedHookState: initialState,
+      next: null
+    }
+    if (!fiber.memoizedState) {
+      fiber.memoizedState = hook;
+    } else {
+      workInProgressHook.next = hook;
+    }
+    workInProgressHook = hook;
+  } else {
+    hook = workInProgressHook;
+    workInProgressHook = workInProgressHook.next;
+  }
+
+  let baseState = hook.memoizedHookState;
+  if (hook.queue.pending) {
+    let firstUpdate = hook.queue.pending.next;
+
+    do {
+      const action = firstUpdate.action;
+      baseState = action(baseState);
+      firstUpdate = firstUpdate.next;
+    } while (firstUpdate !== hook.queue.pending)
+
+    hook.queue.pending = null;
+  }
+  hook.memoizedHookState = baseState;
+
+  return [baseState, dispatchAction.bind(null, hook.queue)];
+}
+
+function App() {
+  const [num, updateNum] = useState(0);
+
+  console.log(`${isMount ? 'mount' : 'update'} num: `, num);
+
+  return {
+    click() {
+      updateNum(num => num + 1);
+    }
+  }
+}
+
+window.app = schedule();
